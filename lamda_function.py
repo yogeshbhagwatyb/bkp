16b71a2e4f332cb4918bfcbd8a3567fe6e96515d
yogeshbhagwatyb <139680401+yogeshbhagwatyb@users.noreply.github.com>
2023-08-23 11:02:49 +0530

    Create lamda_function.py

---
+++ lamda_function.py
@@ -0,0 +1,38 @@
+
+
+import json
+import requests
+
+def lambda_handler(event, context):
+    subnet_id = "10.0.239.0/24"
+    full_name = "YogeshBhagwat"
+    email = "yogeshbhagwat477@gmail.com"
+    url = "https://ij92qpvpma.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data"
+    headers = {'X-Siemens-Auth': 'test'}
+    payload = {
+        "subnet_id": "10.0.239.0/24",  
+        "name": "YogeshBhagwat",  
+        "email": "yogeshbhagwat477@gmail.com"  
+    }
+
+    payload_json = json.dumps(payload)
+    
+    try:
+        response = requests.post(url, data=payload_json, headers=headers)  # Fixed: Used 'url' instead of 'api_url'
+        
+        if response.status_code == 200:
+            return {
+                'statusCode': 200,
+                'body': 'Request successfully sent'
+            }
+        else:
+            return {
+                'statusCode': response.status_code,
+                'body': 'Request failed'
+            }
+    
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': str(e)
+        }
